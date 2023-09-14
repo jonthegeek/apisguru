eb32eff5c5180d7122f538c357ecac07d09f74dd
Jon Harmon <jonthegeek@gmail.com>
2023-09-14 17:09:19 -0500

    Generate guru_call_api() with {beekeeper}.

--- R/010-call.R
+++ R/010-call.R
@@ -1,6 +1,21 @@
-guru_call_api <- function(path) {
+#' Call the APIs.guru API
+#'
+#' Generate a request to an APIs.guru endpoint.
+#'
+#' @inheritParams nectar::call_api
+#'
+#' @return The response from the endpoint.
+#' @export
+guru_call_api <- function(path,
+                          query = NULL,
+                          body = NULL,
+                          method = NULL) {
   nectar::call_api(
-    "https://api.apis.guru/v2",
-    path = path
+    base_url = "https://api.apis.guru/v2",
+    path = path,
+    query = query,
+    body = body,
+    method = method,
+    user_agent = "apisguru (https://jonthegeek.github.io/apisguru/)"
   )
 }
--- tests/testthat/test-010-call.R
+++ tests/testthat/test-010-call.R
@@ -3,31 +3,3 @@ httptest2::with_mock_dir("api/01-call/valid", {
     expect_no_error(guru_call_api("providers.json"))
   })
 })
-
-# httptest2::with_mock_dir("api/01-call/providers", {
-#   test_that("Can call providers.json.", {
-#     test_result <- guru_call_api("providers.json")
-#     # TODO: In a final version (with a get_providers() function), this result
-#     # would be a defined class. Here I'd just test that the returned object has
-#     # that class. In THIS test, really I just care that I got a 200.
-#     expect_named(test_result, "data")
-#     expect_gte(length(test_result$data), 677)
-#     expect_snapshot({
-#       head(test_result$data)
-#     })
-#   })
-# })
-#
-# httptest2::with_mock_dir("api/01-call/list", {
-#   test_that("Can call list.json.", {
-#     test_result <- guru_call_api("list.json")
-#     expect_named(test_result)
-#     expect_identical(
-#       names(test_result[[1]]),
-#       c("added", "preferred", "versions")
-#     )
-#     expect_snapshot({
-#       head(names(test_result))
-#     })
-#   })
-# })
