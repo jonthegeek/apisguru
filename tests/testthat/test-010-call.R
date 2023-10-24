17a52b4ac09806844d412b9018135bd77178b17e
Jon Harmon <jonthegeek@gmail.com>
2023-10-24 14:46:18 -0500

    Clean up.

--- tests/testthat/test-010-call.R
+++ tests/testthat/test-010-call.R
@@ -1,5 +1,5 @@
 httptest2::with_mock_dir("api/01-call/valid", {
   test_that("Can call an endpoint without errors", {
-    expect_no_error(guru_call_api("providers.json"))
+    expect_no_error(call_guru_api("providers.json"))
   })
 })
