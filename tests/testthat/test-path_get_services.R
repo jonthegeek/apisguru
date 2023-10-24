b4336771db697177f91e25f021588db4343fbd24
Jon Harmon <jonthegeek@gmail.com>
2023-10-24 17:10:41 -0500

    Test get_services().

---
+++ tests/testthat/test-path_get_services.R
@@ -0,0 +1,13 @@
+with_mock_dir("api/path/get_services", {
+  test_that("get_services() returns expected result", {
+    test_result <- get_services("adyen.com")
+    expect_s3_class(test_result, "guru_services")
+    expect_gte(length(test_result), 25)
+    expect_contains(
+      test_result,
+      c("BalancePlatformConfigurationNotification-v1", "BalancePlatformService",
+        "HopService", "MarketPayNotificationService", "PayoutService",
+        "TfmAPIService")
+    )
+  })
+})
